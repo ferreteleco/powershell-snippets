dfbdf8801a1a3c7b8349774fff6ae0c063e7d8d1
Andrés Ferreiro González <aferreiro@gradiant.org>
2019-07-01 12:16:51 +0200

    Changes snippet docs

--- src/export_extensions.ps1
+++ src/export_extensions.ps1
@@ -1,16 +1,14 @@
-#
 # @file export_extensions.ps1
 # @project PowerShellScripts
 #
-# @author Andrés Ferreiro González (aferreiro@gradiant.org)
-# @company Centro Tecnolóxico de Telecomunicacións de Galicia (GRADIANT)
+# @author Andrés Ferreiro González (andres.ferreiro.glez@gmail.com)
+# @company --
 #
 # Created: Friday, 28th September 2018 5:53:21 pm
 # Last Modified: Friday, 28th September 2018 6:12:24 pm
-# Modified By: Andrés Ferreiro González (aferreiro@gradiant.org)
+# Modified By: Andrés Ferreiro González (andres.ferreiro.glez@gmail.com)
 #
-# Copyright (c) 2018 Centro Tecnolóxico de Telecomunicacións de Galicia (GRADIANT)
-#
+# Copyright (c) 2018 Andrés Ferreiro González
 
 # This script lists the installed extensions in an instance of Visual Studio Code in order to
 # export them to another one.
